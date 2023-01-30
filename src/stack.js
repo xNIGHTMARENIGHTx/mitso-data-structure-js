cd10dc7d2397ecf7d947dea26a10e7287518e260
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-01-30 09:19:24 +0300

    feat: stack complete

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,16 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  #array = [];
+  push(element) {
+    this.#array.unshift(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return undefined || this.#array.shift();
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.#array[0];
   }
 };
